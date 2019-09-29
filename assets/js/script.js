ed77da1fa5df3f832fc5ad2706c5dfa5c35ad5ed
Tengiz Mindiashvili <tengizmindiashvili@gmai.com>
2019-09-28 20:44:36 -0700

    2 pages 99% done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,37 @@
-let burger = document.getElementById('book');
-console.log(burger);
-let menu = document.getElementById('bigmenu')
-console.log(menu);
-burger.addEventListener('click', function () {
-    this.classList.toggle('active');
-    menu.classList.toggle('open')
+let contact = document.getElementById('book_contact');
+let Book = document.getElementById("book");
+ 
+Book.onclick = function() {
+
+if (contact.style.display === 'block'){
+  contact.style.display = 'none'
+}
+else {
+  contact.style.display = "block"
+}
+if(this.textContent === "Book"){
+  this.textContent = "X"
+}
+else {
+  this.textContent = "Book"
+}
+}
+
+
+let check = document.getElementById('check');
+let sendcheck = document.getElementById("checksend");
+let message = document.getElementById("message");
+
+sendcheck.onclick = function() {
+  check.style.display = "block";
+
+}
+
+let hide = document.getElementsByClassName('disable');
+let send = document.getElementById('checksend')
+send.addEventListener('click', function(){
+ for(i=0; i<hide.length; i++){
+   hide[i].style.display = 'none'
+ }
 })
+
