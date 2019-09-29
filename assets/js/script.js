6c71d7b9b184bdb46e9f4fb1b8a15a6c2beec60f
Tengiz Mindiashvili <tengizmindiashvili@gmai.com>
2019-09-29 02:55:17 -0700

    link update 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,43 @@
-let contact = document.getElementById('book_contact');
+let contact = document.getElementById("book_contact");
 let Book = document.getElementById("book");
- 
+
 Book.onclick = function() {
+  if (contact.style.display === "block") {
+    contact.style.display = "none";
+  } else {
+    contact.style.display = "block";
+  }
+  if (this.textContent === "Book") {
+    this.textContent = "X";
+  } else {
+    this.textContent = "Book";
+  }
+};
 
-if (contact.style.display === 'block'){
-  contact.style.display = 'none'
-}
-else {
-  contact.style.display = "block"
-}
-if(this.textContent === "Book"){
-  this.textContent = "X"
-}
-else {
-  this.textContent = "Book"
-}
-}
-
-
-let check = document.getElementById('check');
+let check = document.getElementById("check");
 let sendcheck = document.getElementById("checksend");
 let message = document.getElementById("message");
 
 sendcheck.onclick = function() {
   check.style.display = "block";
+};
 
-}
-
-let hide = document.getElementsByClassName('disable');
-let send = document.getElementById('checksend')
-send.addEventListener('click', function(){
- for(i=0; i<hide.length; i++){
-   hide[i].style.display = 'none'
- }
-})
+let hide = document.getElementsByClassName("disable");
+let send = document.getElementById("checksend");
+send.addEventListener("click", function() {
+  for (i = 0; i < hide.length; i++) {
+    hide[i].style.display = "none";
+  }
+});
 // contact form send
 
-let sent = document.getElementById('contsend')
-let thank = document.getElementById('thank')
+let sent = document.getElementById("contsend");
+let thank = document.getElementById("thank");
 
 sent.onclick = function() {
   thank.style.display = "block";
-  if( thank.style.display == "block"){
-      sent.style.display = "none"
+  if (thank.style.display == "block") {
+    sent.style.display = "none";
   }
+};
 
-}
-// let hidden = document.getElementsByClassName('disable')
-// let cont = document.getElementById ('contsend')
-// cont.onclick = function() {
-
-//   if (hidden.style.display === 'none'){
-//     hidden.style.display = 'block'
-//   }
-//   else {
-//     hidden.style.display = "none"
-//   }
-// }
